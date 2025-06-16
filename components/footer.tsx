cda9b4798f0e6f590eb06a626732a1109bcafdc0
happybear-21 <mr.rudradityathakur@gmail.com>
2025-06-16 10:56:04 +0530

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,16 +4,7 @@ export default function Footer() {
   return (
     <footer className="px-4 py-4 border-t-[1px]">
       <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-        Built by{" "}
-        <Link
-          href="https://github.com/happybear-21"
-          target="_blank"
-          rel="noreferrer"
-          className="font-medium underline underline-offset-4"
-        >
-          happybear-21
-        </Link>
-        . The source code is available on{" "}
+        The source code is available on{" "}
         <Link
           href="https://github.com/happybear-21/airdoc"
           target="_blank"
